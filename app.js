e19d633799b3540e5b8f01e36e5b2eb8c123d076
yulia pivnenko <julia131289@gmail.com>
2020-07-03 10:29:26 +0300

    add prod

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //Middleware
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
 //Routes
