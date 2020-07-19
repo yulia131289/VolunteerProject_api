6ccd85de4386ecae8aeaf2b6c7a5c986134775e9
yulia pivnenko <julia131289@gmail.com>
2020-07-19 18:57:00 +0300

    add error controller

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const dogRouter = require('./routes/dogRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -25,14 +26,6 @@ app.all('*', (req, res, next) => {
 });
 
 //error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
