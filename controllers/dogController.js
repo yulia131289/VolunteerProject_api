e52c96b2b0b756e0b8c9e88e0edf69daedf59a35
yulia pivnenko <julia131289@gmail.com>
2020-07-22 17:50:11 +0300

    modify catchAsync erros

--- controllers/dogController.js
+++ controllers/dogController.js
@@ -1,98 +1,63 @@
 const Dog = require('../models/dogModel');
 const APIFeatures = require('../utils/apiFeatures');
+const CatchAsync = require('../utils/catchAsync');
 
-exports.getAllDogs = async (req, res) => {
-  try {
-    //Execute query
-    const features = new APIFeatures(Dog.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
+exports.getAllDogs = CatchAsync(async (req, res) => {
+  const features = new APIFeatures(Dog.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-    const dogs = await features.query;
+  const dogs = await features.query;
 
-    res.status(200).json({
-      status: 'sucess',
-      results: dogs.length,
-      data: {
-        dogs,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'sucess',
+    results: dogs.length,
+    data: {
+      dogs,
+    },
+  });
+});
 
-exports.getDogWithID = async (req, res) => {
-  try {
-    const dog = await Dog.findById(req.params.id);
+exports.getDogWithID = CatchAsync(async (req, res) => {
+  const dog = await Dog.findById(req.params.id);
 
-    res.status(200).json({
-      status: 'sucsess',
-      data: dog,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'sucsess',
+    data: dog,
+  });
+});
 
-exports.postNewDog = async (req, res) => {
-  try {
-    const newDog = await Dog.create(req.body);
+exports.postNewDog = CatchAsync(async (req, res) => {
+  const newDog = await Dog.create(req.body);
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        dog: newDog,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      data: 'Invalid data send',
-    });
-  }
-};
+  res.status(201).json({
+    status: 'success',
+    data: {
+      dog: newDog,
+    },
+  });
+});
 
-exports.updateDog = async (req, res) => {
-  try {
-    const updatedDog = await Dog.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: 'success',
-      data: {
-        updatedDog,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+exports.updateDog = CatchAsync(async (req, res) => {
+  const updatedDog = await Dog.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedDog,
+    },
+  });
+});
 
-exports.deleteDog = async (req, res) => {
-  try {
-    await Dog.findByIdAndDelete(req.params.id);
+exports.deleteDog = CatchAsync(async (req, res) => {
+  await Dog.findByIdAndDelete(req.params.id);
 
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      data: err,
-    });
-  }
-};
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
